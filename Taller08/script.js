2e341af90c95007bf190145244d54ce7621484fa
Karla Espinoza <134023247+KarlaEspinozaM@users.noreply.github.com>
2023-06-29 00:40:35 -0500

    Delete Taller08 directory

--- Taller08/script.js
+++
@@ -1,9 +0,0 @@
-var div_cajita= document.getElementById('cajita')
-setTimeout(()=> {
-    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
-
-},2000);
-
-setTimeout(()=> {
-    div_cajita.style.left= (div_cajita.style.left +10) + 'px'
-},6000);
